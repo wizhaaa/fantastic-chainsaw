b1ee8467bf64a84205986288f8263656e7d18b81
Colin Wu <45516888+CollinWoo@users.noreply.github.com>
2024-04-11 13:25:10 -0400

    add spreadsheet component

--- client/src/Dashboard.tsx
+++ client/src/Dashboard.tsx
@@ -1,11 +1,25 @@
+import Spreadsheet from "react-spreadsheet";
 import styles from "./dashboard.module.css";
 
 export const Dashboard = () => {
+  const columnLabels = ["Flavour", "Food"];
+  const rowLabels = ["Item 1", "Item 2"];
+
+  const data = [
+    [{ value: "Vanilla" }, { value: "Chocolate" }, {value: ""}, {value:""}],
+    [{ value: "Strawberry" }, { value: "Cookies" }],
+  ];
+
   return (
     <div className={styles.page}>
       <div>Dashboard</div>
       <div> Filters & Selects </div>
       <div> main layout / grid / cells here </div>
+      <Spreadsheet
+        data={data}
+        columnLabels={columnLabels}
+        rowLabels={rowLabels}
+      ></Spreadsheet>
     </div>
   );
 };
