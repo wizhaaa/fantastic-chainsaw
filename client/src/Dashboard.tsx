45231f4af43fe8dc7247225b92aed175f6e7f0e0
Colin Wu <45516888+CollinWoo@users.noreply.github.com>
2024-04-12 11:01:13 -0400

    add basic interface for demo purposes only, probably needs to be scrapped

--- client/src/Dashboard.tsx
+++ client/src/Dashboard.tsx
@@ -1,18 +1,13 @@
-import Spreadsheet from "react-spreadsheet";
-import styles from "./dashboard.module.css";
+import { MainPage } from "./components/main-page";
 
 export const Dashboard = () => {
 
+  //Kevin, make this function load in the correct data, this currently just loads in a 20 * 100 grid of blank cells
   const data = new Array(100)
-    .fill(null)
-    .map(() => new Array(20).fill({ value: "" }));
+  .fill(null)
+  .map(() => new Array(20).fill({ value: "" }));
 
   return (
-    <div style={{ overflow: "scroll", width: "80vw", height: "100vh" }}>
-
-      <Spreadsheet
-        data={data}
-      ></Spreadsheet>
-    </div>
+      <MainPage data={data}></MainPage>
   );
 };
