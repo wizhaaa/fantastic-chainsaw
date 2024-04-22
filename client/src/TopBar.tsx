81374ab96e46bbb179373c6c277012a4150bde60
Colin Wu <45516888+CollinWoo@users.noreply.github.com>
2024-04-22 00:13:22 -0400

    style text

--- client/src/TopBar.tsx
+++ client/src/TopBar.tsx
@@ -7,23 +7,23 @@ export const TopBar = () => {
       <div className={styles.title}>Table Name</div>
       <div className={styles.bar}>
         <div>
-          <label htmlFor="start">Start</label>
+          <label htmlFor="start">Start:</label>
           <input type="date" id="start" name="start" />
         </div>
 
         <div>
-          <label htmlFor="end">End</label>
+          <label htmlFor="end">End:</label>
           <input type="date" id="end" name="end" />
         </div>
 
         <div>
-          <label htmlFor="confidence">Confidence Level</label>
+          <label htmlFor="confidence">Confidence Level:</label>
           <select id="confidence" name="confidence">
             <option value="95%">95%</option>
           </select>
         </div>
 
-        <div>VaR: 12, 345 USD</div>
+        <div className={styles.var}>VaR: 12, 345 USD</div>
       </div>
     </div>
   );
