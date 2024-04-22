a557a92ae7b83cd9beb4473f97f32d18bba9cb99
Colin Wu <45516888+CollinWoo@users.noreply.github.com>
2024-04-22 11:31:50 -0400

    style inputs

--- client/src/TopBar.tsx
+++ client/src/TopBar.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import { Divider } from "./Divider";
 import styles from "./topbar.module.css";
 //type Props = {}
 
 export const TopBar = () => {
+  const [tableName, setTableName] = useState("Table Name");
+  const [valAtRisk, setValAtRisk] = useState(0);
+
   return (
     <div className={styles.container}>
-      <div className={styles.title}>Table Name</div>
+      <div className={styles.title}>{tableName}</div>
       <div className={styles.bar}>
         <div>
           <label htmlFor="start">Start:</label>
@@ -28,7 +32,7 @@ export const TopBar = () => {
 
         <Divider />
 
-        <div className={styles.var}>VaR: 12, 345 USD</div>
+        <div className={styles.var}>VaR: {valAtRisk}</div>
       </div>
     </div>
   );
