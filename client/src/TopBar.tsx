a37c8817d2eead345bc4ef78468900af8080b3ce
Colin Wu <45516888+CollinWoo@users.noreply.github.com>
2024-04-22 00:04:43 -0400

    add input elements

--- client/src/TopBar.tsx
+++ client/src/TopBar.tsx
@@ -5,7 +5,26 @@ export const TopBar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.title}>Table Name</div>
-      <div className={styles.bar}></div>
+      <div className={styles.bar}>
+        <div>
+          <label htmlFor="start">Start</label>
+          <input type="date" id="start" name="start" />
+        </div>
+
+        <div>
+          <label htmlFor="end">End</label>
+          <input type="date" id="end" name="end" />
+        </div>
+
+        <div>
+          <label htmlFor="confidence">Confidence Level</label>
+          <select id="confidence" name="confidence">
+            <option value="95%">95%</option>
+          </select>
+        </div>
+
+        <div>VaR: 12, 345 USD</div>
+      </div>
     </div>
   );
 };
