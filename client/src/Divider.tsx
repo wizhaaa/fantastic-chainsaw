f12bcc1f26e4ec79b0c63834448fae6aec781f19
Colin Wu <45516888+CollinWoo@users.noreply.github.com>
2024-04-25 00:38:50 -0400

    add new datepicker

--- client/src/Divider.tsx
+++ client/src/Divider.tsx
@@ -20,20 +20,9 @@ export const Divider = () => {
 
 export const DateDivider = () => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="1"
-      height="19"
-      viewBox="0 0 3 34"
-      fill="none"
-    >
-      <path
-        d="M1.48975 1L1.48975 33"
-        stroke="#0955CB"
-        stroke-opacity="0.1"
-        stroke-width="1.5"
-        stroke-linecap="round"
-      />
+    <svg width="20" height="19" viewBox="0 0 2 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <line x1="1" x2="1" y2="20" stroke="black" stroke-opacity="0.1" strokeWidth={1.5}/>
     </svg>
+
   );
 };
--- client/src/TopBar.tsx
+++ client/src/TopBar.tsx
@@ -1,16 +1,14 @@
 import { useState } from "react";
-import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { Divider } from "./Divider";
+import { TopDatePicker } from "./Datepicker";
 import styles from "./topbar.module.css";
 
-//type Props = {}
-
 export const TopBar = () => {
-  const [tableName, ] = useState("Table Name");
-  const [valAtRisk, ] = useState(0);
-  const [startDate, setStartDate] = useState<Date | null>(null);
-  const [endDate, setEndDate] = useState<Date | null>(null);
+  const [tableName] = useState("Table Name");
+  const [valAtRisk] = useState(0);
+  const [startDate, setStartDate] = useState<Date>();
+  const [endDate, setEndDate] = useState<Date>();
 
   return (
     <div className={styles.container}>
@@ -18,22 +16,15 @@ export const TopBar = () => {
       <div className={styles.bar}>
         <div>
           <label htmlFor="start">Start:</label>
-          <DatePicker
-            className={styles.date}
-            placeholderText="MM | DD | YYYY"
-            selected={startDate}
-            onChange={(date: Date | null) => setStartDate(date)}
-          />
+          <TopDatePicker
+            date={startDate}
+            setDate={setStartDate}
+          ></TopDatePicker>
         </div>
 
         <div>
           <label htmlFor="end">End:</label>
-          <DatePicker
-            className={styles.date}
-            placeholderText="MM | DD | YYYY"
-            selected={endDate}
-            onChange={(date: Date | null) => setEndDate(date)}
-          />
+          <TopDatePicker date={endDate} setDate={setEndDate}></TopDatePicker>
         </div>
 
         <Divider />
