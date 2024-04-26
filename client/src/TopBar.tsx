073865533a46b207ac4fc9a69edf1940386c0a73
Colin Wu <45516888+CollinWoo@users.noreply.github.com>
2024-04-25 23:12:12 -0400

    Allow table name to be edited

--- client/src/TopBar.tsx
+++ client/src/TopBar.tsx
@@ -1,30 +1,28 @@
 import { useState } from "react";
 import "react-datepicker/dist/react-datepicker.css";
-import { Divider } from "./Divider";
 import { TopDatePicker } from "./Datepicker";
+import { Divider } from "./Divider";
 import styles from "./topbar.module.css";
+import { DynamicInput } from "./DynamicInput";
 
 export const TopBar = () => {
-  const [tableName] = useState("Table Name");
+  const [tableName, setTableName] = useState("Table Name");
   const [valAtRisk] = useState(0);
   const [startDate, setStartDate] = useState<Date>();
   const [endDate, setEndDate] = useState<Date>();
 
   return (
     <div className={styles.container}>
-      <div className={styles.title}>{tableName}</div>
+      <DynamicInput value={tableName} setValue={setTableName}/>
       <div className={styles.bar}>
         <div>
           <label htmlFor="start">Start:</label>
-          <TopDatePicker
-            date={startDate}
-            setDate={setStartDate}
-          ></TopDatePicker>
+          <TopDatePicker date={startDate} setDate={setStartDate} />
         </div>
 
         <div>
           <label htmlFor="end">End:</label>
-          <TopDatePicker date={endDate} setDate={setEndDate}></TopDatePicker>
+          <TopDatePicker date={endDate} setDate={setEndDate} />
         </div>
 
         <Divider />
