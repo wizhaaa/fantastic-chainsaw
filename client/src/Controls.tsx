cc2a875554d701024254c0a3b10393e11de4e6ee
Will Zhang <wz282@cornell.edu>
2024-04-26 01:11:10 -0400

    var dropdown

--- client/src/Controls.tsx
+++ client/src/Controls.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import styles from "./controls.module.css";
-import { DragOption } from "./Option";
+import {DragOption} from "./Option";
 
 import {Option, FilterRow, QueryType} from "./types";
 
-import { AddModal } from "./AddModal";
-import { Filters } from "./Filters";
+import {AddModal} from "./AddModal";
+import {Filters} from "./Filters";
 
 import addicon from "./assets/add.svg";
 import trashicon from "./assets/trash.svg";
@@ -21,7 +21,7 @@ import {
   useSensor,
   useSensors,
 } from "@dnd-kit/core";
-import { arrayMove, SortableContext } from "@dnd-kit/sortable";
+import {arrayMove, SortableContext} from "@dnd-kit/sortable";
 
 type Display = "none" | "loading" | "data";
 
@@ -306,7 +306,10 @@ export const Controls = (props: PropsType) => {
         filterRows={filterRows}
         setFilterRows={setFilterRows}
       />
-      <button onClick={handleApply}> Apply </button>
+      <button className={styles.applybutton} onClick={handleApply}>
+        {" "}
+        Apply{" "}
+      </button>
     </div>
   );
 
@@ -315,12 +318,12 @@ export const Controls = (props: PropsType) => {
   }
 
   function handleDragStart(event: DragStartEvent) {
-    const { active } = event;
+    const {active} = event;
     setActiveItem(active?.data?.current?.option);
   }
 
   function handleDragMove(event: DragMoveEvent) {
-    const { active, over } = event;
+    const {active, over} = event;
 
     // handle sorting
     if (active && over && active.id !== over.id) {
@@ -340,7 +343,7 @@ export const Controls = (props: PropsType) => {
   }
 
   function handleDragMoveColumn(event: DragMoveEvent) {
-    const { active, over } = event;
+    const {active, over} = event;
 
     // handle sorting
     if (active && over && active.id !== over.id) {
@@ -360,7 +363,7 @@ export const Controls = (props: PropsType) => {
   }
 
   function handleDragEnd(event: DragEndEvent) {
-    const { active, over } = event;
+    const {active, over} = event;
     if (over && active.id !== over.id) {
       console.log("Drag Ending");
     }
--- client/src/TopBar.tsx
+++ client/src/TopBar.tsx
@@ -1,19 +1,23 @@
-import { useState } from "react";
+import {useState} from "react";
 import "react-datepicker/dist/react-datepicker.css";
-import { TopDatePicker } from "./Datepicker";
-import { Divider } from "./Divider";
+import {TopDatePicker} from "./Datepicker";
+import {Divider} from "./Divider";
 import styles from "./topbar.module.css";
-import { DynamicInput } from "./DynamicInput";
+import {DynamicInput} from "./DynamicInput";
 
 export const TopBar = () => {
   const [tableName, setTableName] = useState("Table Name");
   const [valAtRisk] = useState(0);
   const [startDate, setStartDate] = useState<Date>();
   const [endDate, setEndDate] = useState<Date>();
+  const [VaR, setVaR] = useState("90%");
+  const [show, setShow] = useState(false);
+
+  // const [var, setVar] = useState<String>("90");
 
   return (
     <div className={styles.container}>
-      <DynamicInput value={tableName} setValue={setTableName}/>
+      <DynamicInput value={tableName} setValue={setTableName} />
       <div className={styles.bar}>
         <div>
           <label htmlFor="start">Start:</label>
@@ -27,11 +31,64 @@ export const TopBar = () => {
 
         <Divider />
 
-        <div>
+        <div className={styles.wrap}>
           <label htmlFor="confidence">Confidence Level:</label>
-          <select id="confidence" name="confidence">
+          <div className={styles.varwrapper}>
+            {show && (
+              <div
+                className={styles.droppeddown}
+                onClick={() => setShow(!show)}
+              >
+                {" "}
+                {VaR}{" "}
+              </div>
+            )}
+            {!show && (
+              <div className={styles.dropdown} onClick={() => setShow(!show)}>
+                {" "}
+                {VaR}{" "}
+              </div>
+            )}
+
+            {show && (
+              <div className={styles.menu}>
+                <div
+                  className={styles.item}
+                  onClick={() => {
+                    setVaR("90%");
+                    setShow(!show);
+                  }}
+                >
+                  {" "}
+                  90%{" "}
+                </div>
+                <div
+                  className={styles.item}
+                  onClick={() => {
+                    setVaR("95%");
+                    setShow(!show);
+                  }}
+                >
+                  {" "}
+                  95%{" "}
+                </div>
+                <div
+                  className={styles.item}
+                  onClick={() => {
+                    setVaR("99%");
+                    setShow(!show);
+                  }}
+                >
+                  {" "}
+                  99%{" "}
+                </div>
+              </div>
+            )}
+          </div>
+
+          {/* <select id="confidence" name="confidence">
             <option value="95%">95%</option>
-          </select>
+          </select> */}
         </div>
 
         <Divider />
